df9cfb6355609412966fed4a52078352f1bfda3a
Stefan Gebhardt <stefan.gebhardt@finux.net>
2011-12-07 14:57:11 +0100

    Revision 1

--- jquery.mobile.actionsheet.js
+++ jquery.mobile.actionsheet.js
@@ -5,7 +5,7 @@
  * Dual licensed under the MIT and GPL Version 2 licenses.
  * 
  * Date: 2011-05-03 17:11:00 (Tue, 3 May 2011)
- * Revision: 1.1
+ * Revision: 1
  */
 (function($,window){
 	$.widget("mobile.actionsheet",$.mobile.widget,{
